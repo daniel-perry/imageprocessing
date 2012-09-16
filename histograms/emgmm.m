8eff9b6988e3427a43af0844d4c5717da1e412a9
Danny Perry <daniel.perry@gmail.com>
2012-09-16 00:55:11 -0600

    adding em-gmm method to model data and find threshold

--- histograms/emgmm.m
+++ histograms/emgmm.m
@@ -1,3 +1,4 @@
+% Fit a GMM to the data using Expectation-Maximization
 
 % Seg - labeled segmentation over the image
 % Thodl - list of thresholds dividing each label group (gaussian)
@@ -6,30 +7,56 @@
 % max_iters - max # iterations
 function [Seg,Thold] = emgmm( I , k , max_iters)
 
-x = I(:);
+x = I(:)';
+n = size(I,2);
 
 means=stdnormal_rnd(k,1); 
 variances=stdnormal_rnd(k,1); 
 mix=stdnormal_rnd(k,1); 
 mix = mix ./ norm(mix,2);
 
-membership = zeros(n,k);
+membership = zeros(k,n);
 
-for( iters=1:max_iters )
+premeans = means;
+prevariances = variances;
+premix = mix;
+
+iters = 0;
+
+while( iters<max_iters )
 
 	% E-step (membership):
-	for( pt=1:size(x,1) )
+	for( pt=1:size(x,2) )
 		normalizer = 0;
 		for( cluster=1:k )
-			membership(pt,cluster) = mix(cluster) * lognpdf(x(pt),means(cluster),variances(cluster));
-			normalizer += membership(pt,cluster);
+			membership(cluster,pt) = mix(cluster) * lognpdf(x(pt),means(cluster),variances(cluster));
+			normalizer += membership(cluster,pt);
 		end
-		membership(pt,:) ./= normalizer;
+		membership(:,pt) ./= normalizer;
 	end
 
 	% M-step (parameters):
 	n = sum(membership,1); % total responsibility over all pts from each gaussian.
+	means = sum((ones(k,1)*x) .* membership,1) ./ n;
+	dif = ones(k,1)*x - means*ones(size(x));
+	variances = sum(dif .* dif .* membership, 1) ./ n;
+	mix = n ./ size(x,2);
 
-	
+	mnorm = norm(premeans-means);
+	vnorm = norm(prevariances-variances);
+	mixnorm = norm(premix-mix);
+	if( mnorm < 1 && vnorm < 1 && mixnorm < 1 )
+		break;
+  end
 
+  iters+=1
 end
+
+sprintf("iters=%d",iters);
+
+% estimate intersection of gaussians:
+middle = (means(0)+means(1))/2;
+
+Thold = middle;
+Seg = I > Thold;
+
