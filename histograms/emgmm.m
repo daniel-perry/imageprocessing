6f394c5b2944a5ff7d328c9e8d4af5bcd7e69ca8
Danny Perry <daniel.perry@gmail.com>
2012-09-17 20:41:13 -0600

    more progress on em-gmm

--- histograms/emgmm.m
+++ histograms/emgmm.m
@@ -7,13 +7,25 @@
 % max_iters - max # iterations
 function [Seg,Thold] = emgmm( I , k , max_iters)
 
-x = I(:)';
-n = size(I,2);
+x = double(I(:)');
+n = size(x,2);
+size_of_x = size(x)
 
 means=stdnormal_rnd(k,1); 
+
+means *= 10
 variances=stdnormal_rnd(k,1); 
+minvar = min(variances);
+if( minvar < 0)
+  variances -= minvar; %scale to positive..
+end
+variances *= 10;
 mix=stdnormal_rnd(k,1); 
-mix = mix ./ norm(mix,2);
+minmix = min(mix);
+if( minmix < 0 )
+	mix -= minmix; %scale to positive..
+end
+mix = mix ./ norm(mix,1);
 
 membership = zeros(k,n);
 
@@ -26,25 +38,48 @@ iters = 0;
 while( iters<max_iters )
 
 	% E-step (membership):
-	for( pt=1:size(x,2) )
-		normalizer = 0;
-		for( cluster=1:k )
-			membership(cluster,pt) = mix(cluster) * lognpdf(x(pt),means(cluster),variances(cluster));
-			normalizer += membership(cluster,pt);
-		end
-		membership(:,pt) ./= normalizer;
-	end
+	summix = sum(mix)
+	mixmat = mix * ones(1,n); % [mix mix ... mix]
+	meanmat = means * ones(1,n); % [means means ... means]
+	varmat = variances * ones(1,n); % [variances variances .. variances]
+	xmat = ones(k,1) * x; % [x; x; ... x]
+	sumxmat = sum(xmat(:))
+	summeamat = sum(meanmat(:))
+	sumvarmat = sum(varmat(:))
+	summixmat = sum(mixmat(:))
+	membership = mixmat .* normpdf( xmat, meanmat, varmat );
+	summemb = sum(membership(:))
+	% normalize:
+	normalizer = sum(membership,1); % 1 by n
+	minnormalizer = min(normalizer)
+	normalizer = ones(k,1) * normalizer; % k by n
+	membership ./= normalizer;
+	summemb = sum(membership(:))
+
+	%for( pt=1:size(x,2) )
+	%	normalizer = 0;
+	%	for( cluster=1:k )
+	%		membership(cluster,pt) = mix(cluster) * normpdf(x(pt),means(cluster),variances(cluster));
+	%		normalizer += membership(cluster,pt);
+	%	end
+	%	membership(:,pt) ./= normalizer;
+	%end
 
 	% M-step (parameters):
-	n = sum(membership,1); % total responsibility over all pts from each gaussian.
-	means = sum((ones(k,1)*x) .* membership,1) ./ n;
-	dif = ones(k,1)*x - means*ones(size(x));
-	variances = sum(dif .* dif .* membership, 1) ./ n;
-	mix = n ./ size(x,2);
+	normalizer = sum(membership,2) % total responsibility over all pts from each gaussian.
+	means = sum((ones(k,1)*x) .* membership,2) ./ normalizer;
+	dif = (ones(k,1)*x) - (means*ones(1,n));
+	variances = sum(dif .* dif .* membership, 2) ./ normalizer;
+	mix = normalizer ./ n;
 
-	mnorm = norm(premeans-means);
-	vnorm = norm(prevariances-variances);
-	mixnorm = norm(premix-mix);
+	mnorm = norm(premeans-means)
+	mpresum = sum(premeans)
+	msum = sum(means)
+	premeans = means;
+	vnorm = norm(prevariances-variances)
+	prevariances = variances;
+	mixnorm = norm(premix-mix)
+	premix = mix;
 	if( mnorm < 1 && vnorm < 1 && mixnorm < 1 )
 		break;
   end
