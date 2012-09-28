2d7b9b5ace192b3389dfeebdaaf691c01f786a77
Danny Perry <daniel.perry@gmail.com>
2012-09-28 17:13:06 -0600

    renamed more appropriately

--- spatial_filtering/makeBox2D.m
+++ spatial_filtering/makeBox2D.m
@@ -1,6 +1,6 @@
 % create a box filter with side size of SideSize
 
-function Mask = makeBox( SideSize )
+function Mask = makeBox2D( SideSize )
 
 Mask = ones(SideSize,SideSize);
 Mask = (1.0/(SideSize*SideSize)) * Mask;
