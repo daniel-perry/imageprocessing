f62ed2919cb565caf1de434c7eebbaf1bc4516ee
Danny Perry <daniel.perry@gmail.com>
2012-11-13 00:14:44 -0700

    completed and writeup done up to affine correspondence transforms

--- transformations/code/affine_from_points.m
+++ transformations/code/affine_from_points.m
@@ -7,9 +7,45 @@ im2_y = dlmread(sprintf('%s%s',points_prefix,'_im2_y.out'));
 
 % X * A = Y, solve for A
 
-X = [im1_x; im1_y; ones(size(im1_x))];
-Y = [im2_x; im2_y; ones(size(im2_x))];
+%X = [im1_x; im1_y; ones(size(im1_x))];
+%Y = [im2_x; im2_y; ones(size(im2_x))];
+X = [im1_x; im1_y];
+Y = [im2_x; im2_y];
+A = eye(3,3);
 
-A = X\Y;
+if size(X) == [2 3]
+  %rot = Y * inv(X);
+  %rot = [X;1 1 1]\[Y;1 1 1]
+  A_tilde = [ Y(:,1)-Y(:,2) Y(:,1)-Y(:,3) ];
+  P = [ X(:,1)-X(:,2) X(:,1)-X(:,3) ];
+  %rot = A_tilde * inv(P);
+  rot = P \ A_tilde;
+  tran = Y(:,1) - rot * X(:,1);
+  A(1:2,1:2) = rot;
+  A(1:2,3) = tran';
+else % overconstrained
+  X_mean = mean(X,2);
+  Y_mean = mean(Y,2);
+  X_cent = zeros(size(X));
+  Y_cent = zeros(size(Y));
+  yx = zeros(2,2);
+  xx = zeros(2,2);
+  for i=1:size(X,2)
+    X_cent(:,i) = X(:,i)-X_mean;
+    Y_cent(:,i) = Y(:,i)-Y_mean;
+    % outer product
+    for j=1:2
+      for k=1:2
+        yx(j,k) = yx(j,k) + Y_cent(j,i)*X_cent(k,i);
+        xx(j,k) = xx(j,k) + X_cent(j,i)*X_cent(k,i);
+      end
+    end
+  end
+  %rot = yx * inv(xx);
+  rot = xx\yx;
+  tran = Y_mean - rot * X_mean;
+  A(1:2,1:2) = rot;
+  A(1:2,3) = tran;
+end
 
 
---
+++ transformations/code/main.m
@@ -0,0 +1,77 @@
+% main.m - generates all the images shown in the writeup
+
+% images
+checker_fn = '../images/checkboard.jpg';
+checker = imread(checker_fn);
+face1_fn = '../images/face1.png';
+face1 = rgb2gray(imread(face1_fn));
+face2_fn = '../images/face2.png';
+face2 = rgb2gray(imread(face2_fn));
+face3_fn = '../images/face3.png';
+face3 = rgb2gray(imread(face3_fn));
+
+% Transform from landmarks
+%{
+% get_points(face1_fn, face2_fn, 3, '../data/face_3' )
+show_points(face1_fn, face2_fn, '../data/face_3');
+m = affine_from_points('../data/face_3')
+f2_affine = transform( face2, m, 'linear', false );
+imwrite(f2_affine,'../images/f2_affine.png');
+figure
+imshow(f2_affine)
+%}
+
+% get_points(face1_fn, face2_fn, 5, '../data/face_5' )
+show_points(face1_fn, face2_fn, '../data/face_5');
+m5 = affine_from_points('../data/face_5')
+f2_affine_m5 = transform( face2, m5, 'linear', false );
+imwrite(f2_affine_m5,'../images/f2_affine_m5.png');
+figure
+imshow(f2_affine_m5)
+
+
+% Affine Image Transformations
+%{
+f1_trans_nearest = transform( face1, makeTranslate(50,50), 'nearest', true);
+imwrite(f1_trans_nearest, '../images/f1_trans_nearest.png');
+f1_trans_linear = transform( face1, makeTranslate(50,50), 'linear', true);
+imwrite(f1_trans_linear, '../images/f1_trans_linear.png');
+f1_rot_nearest = transform( face1, makeRotate(45), 'nearest', true);
+imwrite(f1_rot_nearest, '../images/f1_rot_nearest.png');
+f1_rot_linear = transform( face1, makeRotate(45), 'linear', true);
+imwrite(f1_rot_linear, '../images/f1_rot_linear.png');
+
+f1_rot_nearest = transform( face1, makeRotate(45), 'nearest', false);
+imwrite(f1_rot_nearest, '../images/f1_rot_nearest_full.png');
+f1_rot_nearest_zoomed = transform( f1_rot_nearest, makeTranslate(-10,-200)*makeScale(3,3), 'nearest', true);
+imwrite(f1_rot_nearest_zoomed, '../images/f1_rot_nearest_zoomed.png');
+f1_rot_linear = transform( face1, makeRotate(45), 'linear', false);
+imwrite(f1_rot_linear, '../images/f1_rot_linear_full.png');
+% note: we zoom with nearest neighbor on purpose...
+f1_rot_linear_zoomed = transform( f1_rot_linear, makeTranslate(-10,-200)*makeScale(3,3), 'nearest', true);
+imwrite(f1_rot_linear_zoomed, '../images/f1_rot_linear_zoomed.png');
+%}
+
+%{
+f1_scale_nearest = transform( face1, makeScale(0.5,0.5), 'nearest', true);
+imwrite(f1_scale_nearest, '../images/f1_scale_nearest.png');
+f1_scale_linear = transform( face1, makeScale(0.5,0.5), 'linear', true);
+imwrite(f1_scale_linear, '../images/f1_scale_linear.png');
+
+f1_shear_nearest = transform( face1, makeShear(0,0.5), 'nearest', false);
+imwrite(f1_shear_nearest, '../images/f1_shear_nearest.png');
+f1_shear_linear = transform( face1, makeShear(0,0.5), 'linear', false);
+imwrite(f1_shear_linear, '../images/f1_shear_linear.png');
+%}
+
+%{
+imsize = size(face1);
+all = makeTranslate(-imsize(1)/2,-imsize(2)/2)*makeRotate(70)*makeShear(0.5,0)*makeTranslate(imsize(1)/3,imsize(2)/3)
+f1_all_nearest = transform( face1, all, 'nearest', false);
+imwrite(f1_all_nearest, '../images/f1_all_nearest.png');
+f1_all_linear = transform( face1, all, 'linear', false);
+imwrite(f1_all_linear, '../images/f1_all_linear.png');
+%}
+
+
+
