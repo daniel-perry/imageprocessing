af68d70d6f3c7d4468f768aa240ea23b07bc9774
Danny Perry <daniel.perry@gmail.com>
2012-11-30 12:54:27 -0700

    emgmm now works

--- histograms/emgmm.m
+++ histograms/emgmm.m
@@ -3,7 +3,7 @@
 % I - input image
 % k - number of gaussians
 % max_iters - max # iterations
-function [Means,Variances,Mix,Membership] = emgmm( I , k , max_iters)
+function [Means,Variances,Mix,Likelihood,Membership] = emgmm( I , k , max_iters)
 
 x = double(I(:)');
 n = size(x,2);
@@ -11,7 +11,7 @@ n = size(x,2);
 % initial mean:
 %means=unifrnd(min(x),max(x),[k,1]);
 %means=uniformrnd(min(x),max(x),[k,1]);
-means = kmeans(I,k,max_iters); % initialize with kmeans result
+means = kmeans(I,k,max_iters) % initialize with kmeans result
 
 % initial variance
 %variances=unifrnd(1,5,[k,1]); 
@@ -23,74 +23,75 @@ mix = ones(k,1);
 mix = mix ./ norm(mix,1);
 
 % initial membership 
-membership = zeros(k,n);
+[membership,likelihood] = expectation( x, means, variances, mix );
 
 % for checking convergence...
-premeans = means
-prevariances = variances
-premix = mix
+prelike = likelihood
 iters = 0;
 
+epsilon = 1e-10;
+
 for( iters=1:max_iters )
-
-	%======================
-	% E-step (membership):
-	%======================
-	mixmat = mix * ones(1,n); % [mix mix ... mix]
-	meanmat = means * ones(1,n); % [means means ... means]
-	varmat = variances * ones(1,n); % [variances variances .. variances]
-	xmat = ones(k,1) * x; % [x; x; ... x]
-	%membership = mixmat .* normpdf( xmat, meanmat, varmat );
-	membership = mixmat .* normalpdf( xmat, meanmat, varmat );
-	% normalize:
-	mem_normalizer = sum(membership,1); % n by 1
-	mem_normalizer = ones(k,1) * mem_normalizer; %* ones(1,n); % k by n
-	membership = membership ./ mem_normalizer;
-  %membership( mem_normalizer <= eps ) = 0 % hack to avoi NaNs
-
 
 	%=======================
 	% M-step (parameters):
 	%======================
-	normalizer = sum(membership,2) % total responsibility over all pts from each gaussian.
-  memb_normed = membership ./ (normalizer*ones(1,n));
-	means = sum((ones(k,1)*x) .* memb_normed,2) ./ normalizer
-	%means = sum((ones(k,1)*x) .* membership,2) ./ normalizer
-  mn2 = memb_normed .* memb_normed;
-  mn2 = ones(k,1) ./ (ones(k,1)-sum(mn2,2));
-	dif = (ones(k,1)*x) - (means*ones(1,n));
-	variances = mn2 .* sum(dif .* dif .* memb_normed, 2) 
-	%variances = sum(dif .* dif .* membership, 2) ./ normalizer
-	mix = normalizer ./ n
+  [means,variances,mix] = maximization( x, membership );
+
+	%======================
+	% E-step (membership):
+	%======================
+  [membership,likelihood] = expectation( x, means, variances, mix );
 
 	%====================
 	% check convergence
 	%====================
-	mnorm = norm(premeans-means);
-	vnorm = norm(prevariances-variances);
-	mixnorm = norm(premix-mix);
-  epsilon = 10^(-3);
-	if( mnorm < epsilon && vnorm < epsilon && mixnorm < epsilon )
-		break;
+  diff = likelihood - prelike;
+  if( abs(diff) < epsilon )
+    break;
   end
 	% save current params for next iteration...
-	premeans = means;
-	prevariances = variances;
-	premix = mix;
-
+  prelike = likelihood;
   iters = iters + 1;
 end
 
-sprintf('iters=%d',iters)
+iters=iters
 
 Means = means;
 Variances = variances;
 Mix = mix;
 Membership = membership;
+Likelihood = likelihood;
 
-% estimate intersection of gaussians:
-%middle = (means(1)+means(2))/2 ;
-%
-%Thold = middle;
-%Seg = I > Thold;
-%
+% calculate membership given the GMM parameters:
+function [membership,likelihood] = expectation(x,means,variances,mix)
+  n = size(x,2);
+  k = size(means,1);
+	mixmat = mix * ones(1,n); % [mix mix ... mix]
+	meanmat = means * ones(1,n); % [means means ... means]
+	varmat = variances * ones(1,n); % [variances variances .. variances]
+	xmat = ones(k,1) * x; % [x; x; ... x]
+	membership = mixmat .* normalpdf( xmat, meanmat, varmat );
+  likelihood = sum(membership(:)) / n; % avg likelihood
+	% normalize:
+	mem_normalizer = sum(membership,1); % n by 1
+	mem_normalizer = ones(k,1) * mem_normalizer; %* ones(1,n); % k by n
+	membership = membership ./ mem_normalizer;
+  membership( mem_normalizer <= eps ) = 0; % hack to avoid NaNs
+
+% calculate the GMM parameters given the membership
+function [means,variances,mix] = maximization( x, membership )
+  n = size(x,2);
+  k = size(membership,1);
+	normalizer = sum(membership,2); % total responsibility over all pts from each gaussian.
+  memb_normed = membership ./ (normalizer*ones(1,n));
+	%means = sum((ones(k,1)*x) .* memb_normed,2) ./ normalizer;
+	means = sum((ones(k,1)*x) .* membership,2) ./ normalizer;
+  mn2 = memb_normed .* memb_normed;
+  mn2 = ones(k,1) ./ (ones(k,1)-sum(mn2,2));
+	dif = (ones(k,1)*x) - (means*ones(1,n));
+	%variances = mn2 .* sum(dif .* dif .* memb_normed, 2) 
+	variances = sum(dif .* dif .* membership, 2) ./ normalizer;
+	mix = normalizer ./ n;
+
+
--- histograms/normalpdf.m
+++ histograms/normalpdf.m
@@ -1,10 +1,9 @@
 % multivariate normal distribution pdf
 % x - point
 % m - mean
-% sd - standard deviation
-function p = normalpdf(x,m,sd)
-
-v = sd.*sd;
+% v - variance
+function p = normalpdf(x,m,v)
+sd = sqrt(v);
 o = ones(size(sd));
 scale_part = o ./ (sd * sqrt(2*pi));
 e_part = exp( -0.5 * ( (x-m).^2 ./ v ) );
