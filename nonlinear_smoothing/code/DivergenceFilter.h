094a732557285ddb3d5b0ce36e176231dcc040e8
Danny Perry <daniel.perry@gmail.com>
2013-02-25 10:29:24 -0700

    adding divergence filter

---
+++ nonlinear_smoothing/code/DivergenceFilter.h
@@ -0,0 +1,77 @@
+/*========================================================================= 
+ * 
+ *  Copyright (c) 2013 Daniel Perry 
+ * 
+ *  MIT license 
+ * 
+ *=========================================================================*/ 
+#ifndef DivergenceFilter_h 
+#define DivergenceFilter_h 
+
+// itk includes
+#include "itkImage.h"
+#include "itkVector.h"
+#include "itkImageToImageFilter.h"
+#include "itkSmartPointer.h"
+
+namespace imageprocessing
+{
+/**
+ * DivergenceFilter
+ *  Takes the one-sided divergence it at each gradient.
+ *  Initially done for 
+ *  CS 7640 - Advanced Image Processing, Spring 2013, University of Utah
+ */
+template< class TInputImage,
+          class TOutputImage=itk::Image<float,TInputImage::ImageDimension>
+        >
+class DivergenceFilter:
+  public itk::ImageToImageFilter<TInputImage,TOutputImage>
+{
+public:
+  /**
+   * Standard class typedefs
+   */
+  typedef DivergenceFilter Self;
+  typedef itk::ImageToImageFilter< TInputImage, TOutputImage > Superclass;
+  typedef itk::SmartPointer< Self > Pointer;
+  typedef itk::SmartPointer< const Self > ConstPointer;
+
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      TInputImage::ImageDimension);
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(DivergenceFilter, ImageToImageFilter);
+
+  /** Image type typedef support. */
+  typedef TInputImage InputImageType;
+  typedef TOutputImage OutputImageType;
+  typedef typename OutputImageType::RegionType OutputImageRegionType;
+  typedef typename InputImageType::SizeType ImageSizeType;
+
+protected:
+  DivergenceFilter()
+  {}
+
+  virtual ~DivergenceFilter(){}
+  void PrintSelf(std::ostream & os, itk::Indent indent) const;
+
+  /**
+   * Standard parallel pipeline method
+   */
+  void BeforeThreadedGenerateData();
+  void ThreadedGenerateData( const OutputImageRegionType & outputRegionForThread,
+                              itk::ThreadIdType threadId);
+
+private:
+  DivergenceFilter(const Self &); // not allowed
+  void operator=(const Self &); // not allowed 
+};
+} // end namespace itk
+
+#include "DivergenceFilter.hxx"
+
+#endif
---
+++ nonlinear_smoothing/code/DivergenceFilter.hxx
@@ -0,0 +1,76 @@
+/*========================================================================= 
+ * 
+ *  Copyright (c) 2013 Daniel Perry 
+ * 
+ *  MIT license 
+ * 
+ *=========================================================================*/ 
+
+#ifndef DivergenceFilter_hxx
+#define DivergenceFilter_hxx
+
+#include "itkConstNeighborhoodIterator.h"
+#include "itkNeighborhoodIterator.h"
+#include "itkImageRegionConstIterator.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageRegionConstIteratorWithIndex.h"
+#include "itkImageRegionIteratorWithIndex.h"
+#include "itkProgressReporter.h"
+
+namespace imageprocessing
+{
+
+template< class TInputImage, class TOutputImage >
+void
+DivergenceFilter< TInputImage, TOutputImage >
+::BeforeThreadedGenerateData()
+{
+}
+
+template< class TInputImage, class TOutputImage >
+void
+DivergenceFilter< TInputImage, TOutputImage >
+::ThreadedGenerateData( const OutputImageRegionType & outputRegionForThread,
+                        itk::ThreadIdType threadId)
+{
+  typedef typename InputImageType::PixelType GradientType;
+  typedef typename InputImageType::SizeType SizeType;
+  typedef typename InputImageType::IndexType IndexType;
+  typedef typename OutputImageType::PixelType PixelType;
+
+  typename InputImageType::ConstPointer input = this->GetInput();
+  typename OutputImageType::Pointer output = this->GetOutput();
+  SizeType size = input->GetLargestPossibleRegion().GetSize();
+  
+  itk::ImageRegionConstIteratorWithIndex<InputImageType> it(input, outputRegionForThread);
+  itk::ImageRegionIterator<OutputImageType> out(output, outputRegionForThread);
+  for(it.GoToBegin(),out.GoToBegin(); !it.IsAtEnd(); ++it,++out)
+  {
+    PixelType div = 0;
+    GradientType grad = it.Get();
+    IndexType center = it.GetIndex();
+    for(size_t i=0; i<grad.Size(); ++i)
+    {
+      if(!(center[i] == 0 || center[i] == size[i]-1)) // on edge
+      {
+        IndexType overOne = center;
+        overOne[i] -= 1;
+        GradientType gradOverOne = input->GetPixel(overOne);
+        div += gradOverOne[i] - grad[i];
+      }
+    }
+    output->SetPixel( out.GetIndex(), div );
+  }
+}
+
+template< class TInputImage, class TOutputImage >
+void
+DivergenceFilter< TInputImage, TOutputImage >
+::PrintSelf(std::ostream & os, itk::Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+}
+
+} // end namespace
+
+#endif
