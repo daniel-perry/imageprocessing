9379f3d4de76266be6c88421c49e45734ff5c181
Danny Perry <dperry@sci.utah.edu>
2013-04-03 15:29:31 -0600

    kSVD mostly there

--- sparse_coding/code/SparseDictionary.jl
+++ sparse_coding/code/SparseDictionary.jl
@@ -4,12 +4,12 @@
 
 module SparseDictionary
 
-export matchingPursuit,orthogonalPursuit
+export matchingPursuit,orthogonalPursuit,kSVD
 
-# function matchingPursuit(f, d)
+# function matchingPursuit(F, D, lambda)
 #params:
-# f - original signal
-# d - sparse dictionary
+# F - original signal
+# D - sparse dictionary
 # lambda - number of elements in sparse representation
 #returns:
 # sparse representation
@@ -18,29 +18,92 @@ function matchingPursuit(F, D, lambda)
   F = F[:]
   R = F # initial residual
 
-  gamma = zeros(size(D,2))
+  x = zeros(size(D,2))
   for n=1:lambda
+    #print(n,",")
     max_similarity = 0.0
     max_i = 0.0
     for i=1:size(D,2)
-      if gamma[i] == 0 # this dictionary element not already used..
+      if x[i] == 0 # this dictionary element not already used..
         g = D[:,i]
         similarity = (g' * R)[1]
-        if similarity > max_similarity
+        if abs(similarity) > (max_similarity)
           max_similarity = similarity
           max_i = i
         end
       end
     end
-    gamma[max_i] = max_similarity
+    x[max_i] = max_similarity
     #update residual:
-    R = D*gamma - F
+    R = D*x - F
   end
-  gamma
+  x
 end # end function
 
 function orthogonalPursuit(F,D,lambda)
   println("Not yet implemented..")
+end # end function
+
+# function kSVD(F, D, lambda)
+#params:
+# F - original signal, F= [ F_1 F_2 ... F_k ] , F_i \in R^l            (l,k)
+# D - initial sparse dictionary D = [ D_1 D_2 ... D_n ] , D_i \in R^l  (l,n)
+# lambda - number of elements in sparse representation
+# maxIters - maximum iterations
+#returns:
+# (X, D)
+# X - sparse representation, X = [ X_1 X_2 ... X_k ], X_i \in R^n    (n,k)
+# D - sparse dictionary                                              (l,n)
+function kSVD(F, D, lambda, maxIters)
+  # make sure columns of D or normalized using L2 norm:
+  for i=1:size(D,2)
+    D[:,i] = D[:,i] / norm(D[:,i],2)
+  end
+
+  l = size(F,1) # size of a single signal or atom
+  n = size(D,2) # number of dictionary atoms
+  k = size(F,2) # number of signals 
+
+  println("INFO: number of atoms = ",n)
+  println("INFO: number of signals = ",k)
+
+  X = zeros(n,k) # sparse representation  nxk
+
+  for i=1:maxIters
+    println("\n",i)
+    # sparse coding step:
+    println("sparse coding step")
+    for j=1:k # each column of X,F
+      if j%100 == 0
+        print(".") # progress...
+      end
+      X[:,j] = matchingPursuit(F[:,j],D,lambda)
+    end
+    # codebook update step:
+    println("\ncodebook update step")
+    for j=1:n # each column of D
+      if j%100 == 0
+        print(".") # progress...
+      end
+      tmp = [X[j,ii] > 0 for ii=1:size(X,2)] # which atoms are being used  (1,k)
+      w = find(tmp)
+      if length(w) > 0 # only update if this atom is used
+        E_total = zeros(l,k)
+        for m=1:n
+          if m != j
+            E_total += D[:,m] * X[m,:]
+          end
+        end
+        E_total = F - E_total
+        E_restricted = E_total[:,w] # (l,nnz)
+        U,S,V = svd( E_restricted )
+        D[:,j] = U[:,1]  # update dictionary column
+        X[j,w] = V[:,1] * S[1,1] # update non-zero elements of row of X
+      end
+    end
+  end
+  println("\nkSVD done.")
+  X,D
 end # end function
 
 end # end module
--- sparse_coding/code/main.jl
+++ sparse_coding/code/main.jl
@@ -6,14 +6,82 @@ include("image.jl") # for reading, displaying images
 
 using SparseDictionary
 
-function main()
-  circle = imread("../images/circle.png")
-  circle = rgb2gray(circle)
+# chunk up an image, into non-overlapping chunks
+function enChunk(image,chunkSize)
+  chunkLength = chunkSize[1] * chunkSize[2]
+  imageSize=size(image)
+  chunks = Array(Float32, (chunkLength,0) )
+  for r=1:chunkSize[1]:imageSize[1]
+    for c=1:chunkSize[2]:imageSize[2]
+      piece = zeros(chunkSize)
+      #println("image size: ",imageSize)
+      #println("image:")
+      #println(r:min(r+chunkSize[1]-1,imageSize[1]))
+      #println(c:min(c+chunkSize[2]-1,imageSize[2]))
+      #println("patch:")
+      #println(1:min(chunkSize[1],imageSize[1]-r+1))
+      #println(1:min(chunkSize[2],imageSize[2]-c+1))
+      piece[1:min(chunkSize[1],imageSize[1]-r+1), 1:min(chunkSize[2],imageSize[2]-c+1)] = image[r:min(r+chunkSize[1]-1,imageSize[1]),c:min(c+chunkSize[2]-1,imageSize[2])]
+      chunks = [chunks piece[:]]
+    end
+  end
+  chunks
+end
 
-  c = circle[:]
-  d = [c*20 c/20 c*50 c/50 c*100 c/100]
-  a = matchingPursuit(c,d,2)
-  println(a)
+# combine non-overlapping chunks into a full image
+function deChunk(X,D,chunkSize,imageSize)
+  image = zeros(imageSize)
+  for r=1:chunkSize[1]:imageSize[1]
+    for c=1:chunkSize[2]:imageSize[2]
+      ind = r*chunkSize[2]+c
+      if ind <= size(X,2)
+        piece = D*X[:,ind]
+        piece = reshape(piece,chunkSize)
+        image[r:min(r+chunkSize[1]-1,imageSize[1]),c:min(c+chunkSize[2]-1,imageSize[2])] = piece[1:min(chunkSize[1],imageSize[1]-r+1), 1:min(chunkSize[2],imageSize[2]-c+1)]
+      end
+    end
+  end
+  image
+end
+
+# create a random dictionary of dimension (atomSize,atomCount)
+# generated following procedure defined in section V of the original k-SVD paper.
+function randomDictionary(atomSize, atomCount)
+  genSize = 50
+  comps = 3
+  gen = rand(atomSize,genSize)
+  for i=1:genSize
+    gen[:,i] = gen[:,i] * (1/norm(gen[:,i],2))
+  end
+  d = zeros(atomSize,atomCount)
+  for i=1:atomCount
+    coeff = rand(comps)'
+    coeff = coeff / sum(coeff)  # random coefficients should sum to 1..
+    loc = rand(Uint32, comps)
+    loc = mod(loc,genSize) + ones(Uint32,comps) # random locations in the generating dictionary
+    parts = gen[:,loc]
+    coeff = ones(comps,1)*coeff
+    atom = sum( parts*coeff, 2 )
+    d[:,i] = atom
+  end
+  d
+end
+
+function main()
+  sailboat = imread("../images/sailboat.png")
+  sailboat = rgb2gray(sailboat)
+
+  chunkSize = (8,8)
+  chunkLength = chunkSize[1] * chunkSize[2]
+
+  f = enChunk( sailboat , chunkSize )
+  d = randomDictionary( chunkLength , 1500 )
+  #a = matchingPursuit(c,d,2)
+  #x,d = kSVD(f[:,1:100],d, 20,2)
+  x,d = kSVD(f,d, 20,10)
+  
+  newsb = deChunk( x,d , chunkSize, size(sailboat) )
+  imwrite( newsb, "tmp.png" )
 end
 
 main() # entry point
